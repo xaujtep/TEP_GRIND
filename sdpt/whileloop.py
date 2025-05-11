598a4920e4fd625065a516882546ea387e39a10b
Tep <stephnez09@gmail.com>
2025-05-11 23:36:20 +0800

    Add files via upload

---
+++ sdpt/whileloop.py
@@ -0,0 +1,30 @@
+
+#while loop with else
+# age = 12
+
+# while age < 18:
+#     print("Still Young: "+ str(age))
+#     age += 1
+# else:
+#     print("Legal age: "+ str(age))
+#=======================================================
+#while loop in collection
+# studentID = [123,1234,12345, 123456 ]
+# i = 0
+
+# while i < len(studentID):
+#     print(studentID[i])
+#     i += 1
+#=======================================================
+#BREAK
+
+# while True:
+#     print("Hello World")
+#     break
+#=======================================================
+#conditions in while loop
+while True:
+    ans = input("Bakit nga ba? ")
+    if ans == "YOU TELL ME!":
+        print("CORRECT")
+        break
